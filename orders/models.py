6b6bc0fbb0b9f098438ebe6be09963279aeed00d
antonnifo <antonnifo@live.com>
2023-04-21 11:17:31 +0300

    add quantity field

--- orders/models.py
+++ orders/models.py
@@ -32,6 +32,8 @@ class OrderItem(models.Model):
                                 on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=10,
                                 decimal_places=2)
+    quantity = models.PositiveIntegerField(default=1)
+                                
                                 
     def __str__(self):
             return str(self.id)
