25ed48be03900e17163e1f6cf2850600853272e2
antonnifo <antonnifo@live.com>
2023-04-23 12:41:59 +0300

    include payments app urls

--- Engine/urls.py
+++ Engine/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('cart/', include('cart.urls', namespace='cart')),
     path('orders/', include('orders.urls', namespace='orders')),
+    path('payment/', include('payment.urls', namespace='payment')),
     path('', include('shop.urls', namespace='shop')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
