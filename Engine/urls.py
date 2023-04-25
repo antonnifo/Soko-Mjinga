bdc87b3b0982a23ebe2954fb09102526c841cc5e
antonnifo <antonnifo@live.com>
2023-04-25 11:01:59 +0300

    include coupons app urls

--- Engine/urls.py
+++ Engine/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     path('cart/', include('cart.urls', namespace='cart')),
     path('orders/', include('orders.urls', namespace='orders')),
     path('payment/', include('payment.urls', namespace='payment')),
+    path('coupons/', include('coupons.urls', namespace='coupons')),
     path('', include('shop.urls', namespace='shop')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
