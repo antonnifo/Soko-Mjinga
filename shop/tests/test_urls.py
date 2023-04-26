61bd2b3bcb1556092cf1ddfaf261448b5abb5419
antonnifo <antonnifo@live.com>
2023-04-26 13:13:53 +0300

    test product detail url

--- shop/tests/test_urls.py
+++ shop/tests/test_urls.py
@@ -41,3 +41,28 @@ class ProductListByCategoryPageURLTests(TestCase):
         url = reverse('shop:product_list_by_category' , args=['electronics'])
         self.assertEquals(resolve(url).func, product_list)
                             
+
+class ProductDetailPageURLTests(TestCase):
+
+    def setUp(self):   
+        self.category = Category.objects.create(name="Electronics",slug="electronics")
+        self.product = Product.objects.create(name="JBL SoundBar",slug ="jbl-soundbar",description="5.1 Channel",\
+                                    category=self.category,
+                                    price=99.99)  
+        
+        self.products_by_category = self.category.products.all() 
+   
+    # def test_url_exists_at_correct_location(self):
+    #     response = self.client.get("/<self.product.id>/jbl-soundbar/")
+    #     print(self.product.id)
+    #     self.assertEqual(response.status_code, 200)
+
+    def test_url_available_by_name(self):  
+        response = self.client.get(reverse("shop:product_detail", args=[self.product.id,\
+                                                                      self.product.slug]))                                                             
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_maps_right_view(self):
+        url = reverse('shop:product_detail' , args=[self.product.id,\
+                                                    self.product.slug])
+        self.assertEquals(resolve(url).func, product_detail)
